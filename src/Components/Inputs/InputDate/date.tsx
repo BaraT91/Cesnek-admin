2ee9b554dd8df1d94ff20cff2d088a150667b78d
Barbora Tesařová <tesarovabara@seznam.cz>
2024-07-08 10:35:45 +0200

    Creating an Input component

---
+++ src/Components/Inputs/InputDate/date.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Date: React.FC = () => (
+<div className="form-group">
+            <label htmlFor="date">Datum:</label>
+            <input type="date" id="date" name="date" />
+          </div>
+);
---
+++ src/Components/Inputs/InputWorker/worker.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const UnitPrice: React.FC = () => (
+  <div className="form-group">
+    <label htmlFor="worker">Pracovník:</label>
+    <input type="text" id="worker" name="worker" />
+  </div>
+);
