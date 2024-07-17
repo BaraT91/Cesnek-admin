aed8c8356f6663180671554c4e28a9a034787b6a
Barbora Tesařová <tesarovabara@seznam.cz>
2024-07-17 16:18:44 +0200

    Emoji in to the links

--- src/Pages/Admin/admin.tsx
+++ src/Pages/Admin/admin.tsx
@@ -3,9 +3,9 @@ import LinkButton from '../../Components/LinkButton/linkButton';
 
 export const Admin: React.FC = () => (
   <div className="container">
-    <LinkButton to="/sprava/pracovnici">Pracovníci</LinkButton>
-    <LinkButton to="/sprava/cinnosti">Činnosti</LinkButton>
-    <LinkButton to="/sprava/prehledy">Editor přehledů</LinkButton>
+    <LinkButton to="/sprava/pracovnici">Pracovníci 👩🏽‍🌾</LinkButton>
+    <LinkButton to="/sprava/cinnosti">Činnosti 🧄</LinkButton>
+    <LinkButton to="/sprava/prehledy">Editor přehledů 👨🏼‍🔧</LinkButton>
   </div>
 );
 export default Admin;
